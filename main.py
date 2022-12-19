b70df911bc57141466a0be0e098f50eb0cf78c80
Vasiliy Samoylik <44618834+ToxicVasiliy@users.noreply.github.com>
2022-12-19 10:05:42 +0300

    Moved tests to different file
    Now its just better

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import unittest
-
 def calculator(a, b, c):
     if c == '+':
         return a + b
@@ -18,28 +16,3 @@ def calculator(a, b, c):
 
 
 
-class TestCalculator(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(calculator(1, 2, '+'), 3)
-
-    def test_subtract(self):
-        self.assertEqual(calculator(1, 2, '-'), -1)
-
-    def test_multiply(self):
-        self.assertEqual(calculator(1, 2, '*'), 2)
-
-    def test_divide(self):
-        self.assertEqual(calculator(1, 2, '/'), 0.5)
-
-    def test_modulo(self):
-        self.assertEqual(calculator(1, 2, '%'), 1)
-
-    def test_exponent(self):
-        self.assertEqual(calculator(1, 2, '**'), 1)
-
-    def test_invalid_operator(self):
-        self.assertEqual(calculator(1, 2, 'x'), "Invalid operator")
-
-
-if __name__ == '__main__':
-    unittest.main()
